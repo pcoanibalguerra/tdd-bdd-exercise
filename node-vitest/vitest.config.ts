c3bddef412a8b086906c85968092c525ee9783e2
Jubel Correa <jubel.correa@gmail.com>
2024-10-20 06:49:39 -0500

    update verson, removed tests to explain coverage

--- node-vitest/vitest.config.ts
+++ node-vitest/vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'v8', // or 'istanbul' for Istanbul
+      enabled: true,
+      provider: 'istanbul', // or 'istanbul' for Istanbul
       reporter: ['text', 'json', 'html'],
       include: ['src/**/*'],
       exclude: ['tests/**/*']
